0e50550d78e76d205f14f039cb5436f86105db21
Sandro Roberto Leiria Sampaio <93958144+Slroberto@users.noreply.github.com>
2025-10-09 07:57:12 -0400

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -54,7 +54,7 @@ export const Header: React.FC<HeaderProps> = ({ onAddOrderClick }) => {
                         value={searchTerm}
                         onChange={(e) => setSearchTerm(e.target.value)}
                         className="w-64 appearance-none bg-black/30 border border-granite-gray/50 rounded-lg pl-10 pr-4 py-2 text-sm text-gray-300 focus:outline-none focus:ring-2 focus:ring-cadmium-yellow"
-                        aria-label="Search orders"
+                        aria-label="Buscar ordens de serviço"
                     />
                 </div>
             )}
